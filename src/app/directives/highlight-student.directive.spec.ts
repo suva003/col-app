c50b961913acc45fb08a5a78bd107eed7d292035
suva003 <suvareeghosh500@gmail.com>
2019-04-29 12:36:27 +0530

    initial commit

---
+++ src/app/directives/highlight-student.directive.spec.ts
@@ -0,0 +1,13 @@
+/**
+ * Created By : Sangwin Gawande (http://sangw.in)
+ */
+
+import { HighlightStudentDirective } from './highlight-student.directive';
+
+describe('HighlightStudentDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightStudentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
+
